1f1e8a5134eea02cece4d7a463cd6f6e917605c5
Marco Tasca <tascamarcoo@gmail.com>
2023-06-07 15:05:05 +0200

    Remove unused import

--- src/main/java/com/bsf/security/config/SecurityConfiguration.java
+++ src/main/java/com/bsf/security/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.bsf.security.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
