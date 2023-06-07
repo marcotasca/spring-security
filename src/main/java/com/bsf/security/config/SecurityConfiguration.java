58a8c834307da4dae5f14157c7f79b2e70674bdc
Marco Tasca <tascamarcoo@gmail.com>
2023-06-07 14:20:28 +0200

    Add the security filter

---
+++ src/main/java/com/bsf/security/config/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.bsf.security.config;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // Disabilito il Cross-Site Request Forgery
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        // Abilita una whitelist di url
+                    .requestMatchers("/api/v1/auth/**")
+                    .permitAll()
+                        // Per tutti gli altri url bisogna essere autenticati
+                    .anyRequest()
+                    .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Forniamo AuthenticationProvider creato in ApplicationConfig
+                .authenticationProvider(authenticationProvider)
+                // Prima di tutto utilizziamo il filtro creato in JwtAuthenticationFilter
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return httpSecurity.build();
+    }
+
+}
