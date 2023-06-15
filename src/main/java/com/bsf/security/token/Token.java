270f8f47f999f2b3ee8463b47367eeb4329f8cd1
Marco Tasca <tascamarcoo@gmail.com>
2023-06-15 16:49:09 +0200

    Remove unused import

--- src/main/java/com/bsf/security/token/Token.java
+++ src/main/java/com/bsf/security/token/Token.java
@@ -1,7 +1,6 @@
 package com.bsf.security.token;
 
 import com.bsf.security.user.User;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
