02a4d24680e898c7a600ac26b1f8c6b5271e0831
Marco Tasca <tascamarcoo@gmail.com>
2023-07-06 17:08:25 +0200

    Delete annotation EnableWebMvc (cause bad format date)

--- src/main/java/com/bsf/security/SecurityApplication.java
+++ src/main/java/com/bsf/security/SecurityApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @SpringBootApplication
 public class SecurityApplication {
 
