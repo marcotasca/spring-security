73a8e6ed23bd1f5385f4def4ff63db3a3a0b55df
Marco Tasca <tascamarcoo@gmail.com>
2023-07-06 16:56:20 +0200

    Disable exception handler for unhandled exception.

--- src/main/java/com/bsf/security/error/UnhandledErrorAdvice.java
+++ src/main/java/com/bsf/security/error/UnhandledErrorAdvice.java
@@ -17,23 +17,23 @@ public class UnhandledErrorAdvice extends ResponseEntityExceptionHandler {
 
     private final BTExceptionResolver btExceptionResolver;
 
-    @ExceptionHandler({Exception.class})
-    public ResponseEntity<BTExceptionResponse> exception(Exception ex) {
-        StackTraceElement[] stackTrace = ex.getStackTrace();
-        if (stackTrace.length > 0) {
-            StackTraceElement firstStackTraceElement = stackTrace[0];
-            String methodName = firstStackTraceElement.getMethodName();
-            int lineNumber = firstStackTraceElement.getLineNumber();
-            String fileName = firstStackTraceElement.getFileName();
-
-            log.info("{}::{}() [Lines: {}] - {}",
-                    fileName == null ? null : fileName.replace(".java", ""),
-                    methodName,
-                    lineNumber,
-                    ex.getMessage()
-            );
-        }
-        return new ResponseEntity<>(new BTExceptionResponse(ex.getMessage(), HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
-    }
+//    @ExceptionHandler({Exception.class})
+//    public ResponseEntity<BTExceptionResponse> exception(Exception ex) {
+//        StackTraceElement[] stackTrace = ex.getStackTrace();
+//        if (stackTrace.length > 0) {
+//            StackTraceElement firstStackTraceElement = stackTrace[0];
+//            String methodName = firstStackTraceElement.getMethodName();
+//            int lineNumber = firstStackTraceElement.getLineNumber();
+//            String fileName = firstStackTraceElement.getFileName();
+//
+//            log.info("{}::{}() [Lines: {}] - {}",
+//                    fileName == null ? null : fileName.replace(".java", ""),
+//                    methodName,
+//                    lineNumber,
+//                    ex.getMessage()
+//            );
+//        }
+//        return new ResponseEntity<>(new BTExceptionResponse(ex.getMessage(), HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
+//    }
 
 }
